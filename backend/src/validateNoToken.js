5c0168bb3d111e0b3c3a37b45d8c969abc823be6
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2022-10-04 19:51:18 -0400

    perf: Import token into functions
    Import token into verifyToken and verifyNoToken functions to improve session between two parties
    
    Crear Express API (Back End)

--- backend/src/validateNoToken.js
+++ backend/src/validateNoToken.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken')
 
 const verifyNoToken = (req, res, next) => {
-    const token = req.header('auth-token')
-    console.log(token)
+    const token = req.session.token
+    //const token = req.header('auth-token')
     if (!token) {
-        console.log(".............No Token")
+        console.log(".............No Token-----> Next")
         return next()
     }
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = verified
-        return res.redirect("/")
+        return res.redirect("/login")
 
     } catch (error) {
         res.status(400).json({error: 'Error de Token'})
