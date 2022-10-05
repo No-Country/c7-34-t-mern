f7763f5a401a74afc9bf13c3d7624fe4ab8c4b20
jfchilito7 <jfchilito7@gmail.com>
2022-10-05 18:28:30 -0500

    feat: :art: Creation of Navbar Desktop and mobile
    Create of Nabvar and adds icons dependences
    
    CB_#22_Navbar CoinBook

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button(props) {
   return (
-    <button className='bg-primary-dark text-white py-2 px-6 rounded md:ml-8 hover:bg-primary-dye duration-500'>
+    <button className='bg-primary-darker text-white py-2 px-6 rounded md:ml-8 hover:bg-primary-dye duration-500'>
         {props.children}
     </button>
   )
