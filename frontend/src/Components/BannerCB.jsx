14e29906fb2b9df166602191cb1f5194bc2961f0
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-06 00:10:37 -0400

    style: Change Banner Style
    Change gap & padding button

--- frontend/src/Components/BannerCB.jsx
+++ frontend/src/Components/BannerCB.jsx
@@ -13,7 +13,7 @@ function BannerCB() {
           />
         </div>
       </section>
-      <section className="flex flex-col justify-center items-center sm:items-left gap-8 sm:gap-6 px-8 md:px-0 pr-12 md:pr-20 ">
+      <section className="flex flex-col justify-center items-start sm:items-left gap-10 px-8 md:px-0 pr-12 md:pr-20 ">
         <article className="w-full font-general font-normal text-justify text-headline leading-lh-7 sm:leading-lh-8 md:text-left md:text-title3 md:leading-lh-9">
           <span className="font-general font-semibold text-bold_title sm:text-[3rem] text-primary-light">
             Coin
@@ -26,10 +26,10 @@ function BannerCB() {
           dividir cuentas con tus amigos. todo lo que quieras controlar nosotros
           <b> te ayudamos</b>
         </article>
-        <div className="">
+        <div>
           <a
             href=""
-            className="bg-primary-light px-4 py-2 rounded-md shadow-xl text-white font-general font-semibold text-title3"
+            className="bg-primary-light px-4 py-3 rounded-md shadow-xl text-white font-general font-semibold text-title3"
           >
             ¡Empieza ahora!
           </a>
