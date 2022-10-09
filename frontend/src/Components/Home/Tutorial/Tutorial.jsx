a032ab67cd447b828d684e77beb4a66a8a547535
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-09 18:59:00 -0400

    refactor: Remove TutorialBg
    Removing TutorialBg component and adding to Tutorial with JSX
    
    Atomic Design & Clean Architecture

--- frontend/src/Components/Home/Tutorial/Tutorial.jsx
+++ frontend/src/Components/Home/Tutorial/Tutorial.jsx
@@ -1,11 +1,19 @@
-import TutorialBg from "./TutorialBg"
+import appImg from "@/assets/cbapp.svg"
 import TutorialText from "./TutorialText"
 
 function Tutorial() {
   return (
     <main className="grid md:grid-cols-2 gap-2 py-12 sm:py-20 mx-auto">
       <TutorialText />
-      <TutorialBg />
+      <section className="bg-bgBanner1 bg-no-repeat bg-contain bg-right-bottom hidden md:block lg:w-9/1">
+        <div className="flex w-full justify-center items-center">
+          <img
+            src={appImg}
+            alt="CB-Banner"
+            className="hidden md:block md:h-[40vh] lg:w-9/12"
+          />
+        </div>
+      </section>
     </main>
   )
 }
