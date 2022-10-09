ca96d31e2eaab8c886d0fc074fe064edb8fab2e1
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-09 01:27:57 -0400

    feat: Add React Router
    Add Routes of the project
    
    Rutas del proyecto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import Home from "./Pages/Home/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home, SignIn } from "./Pages"
 
 function App() {
   return (
-    <div className="">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="home" element={<Home />} />
+        <Route exact path="login" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
