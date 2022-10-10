ed44c45d194df52746e8584c88682b0d1bd33c60
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2022-10-10 16:54:09 -0400

    refactor: API added
    BackEnd API and functions added
    
    Connect Frontend- Backend login (Backend)

--- frontend/src/Components/Login/Form/Form.jsx
+++ frontend/src/Components/Login/Form/Form.jsx
@@ -1,11 +1,49 @@
+import { useState, useEffect } from 'react';
+
 function Form() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //alert("User Added")
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+   try{
+    const add = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }).then(res => {return res.json()});
+
+  console.log(add)
+   }catch(err){
+     console.error()
+   }
+  };
+
   return (
     <aside>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-6">
           <label className="text-footer font-semibold text-[#8E9093] tracking-wide">
             Email
             <input
+              onChange={handleEmail}
               className="w-full text-call text-black py-2 bg-inherit border-b border-black focus:outline-none focus:border-primary-light"
               type="email"
             />
@@ -15,6 +53,7 @@ function Form() {
           <label className="text-footer font-semibold text-[#8E9093] tracking-wide">
             Password
             <input
+              onChange={handlePassword}
               className="w-full text-call text-black py-2 bg-inherit border-b border-black focus:outline-none focus:border-primary-light"
               type="password"
             />
