ab12928c8e1ae2b44b65f93dec1cd43f0f232335
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-10 23:44:13 -0400

    style: Change BenefitsSection style
    Add more padding, change gap & spaces to mobile view
    
    FIX - Variados

--- frontend/src/Components/Home/Benefits/BenefitsSection.jsx
+++ frontend/src/Components/Home/Benefits/BenefitsSection.jsx
@@ -1,55 +1,56 @@
-import { useState } from "react";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
-import { benefits } from "@/helpers/benefits";
+import { benefits } from "@/helpers/benefits"
+import { useState } from "react"
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 
 const BenefitsSection = () => {
-  const [selectedBenefit, setSelectedBenefit] = useState(0);
+  const [selectedBenefit, setSelectedBenefit] = useState(0)
   return (
-    <section className="px-2 lg:p-0 w-full flex flex-col items-center pt-14 md:pb-24 font-general">
-      <h2 className="md:mb-20 text-bold_title font-semibold"> ¿Por qué Utilizar los Bolsillos? </h2>
-      <div className="flex items-center sm:items-start sm:grid grid-cols-2 lg:flex gap-10">
-        <span 
-          className="sm:hidden hover:cursor-pointer" 
+    <section className="bg-[#FAFAFA] p-8 w-full flex flex-col items-center font-general shadow-sm">
+      <h2 className="mb-10 md:mb-20 text-title1 sm:text-bold_title font-semibold font-general">
+        {" "}
+        ¿Por qué Utilizar los Bolsillos?{" "}
+      </h2>
+      <div className="flex items-center sm:items-start sm:grid grid-cols-2 lg:flex sm:gap-10">
+        <span
+          className="sm:hidden hover:cursor-pointer"
           onClick={() => {
-            if(selectedBenefit > 0) setSelectedBenefit(selectedBenefit - 1)
+            if (selectedBenefit > 0) setSelectedBenefit(selectedBenefit - 1)
           }}
         >
-          <IoIosArrowBack 
-            size={52} 
-            color={`${selectedBenefit === 0 
-              ? 'transparent' 
-              : ''
-            }`} 
+          <IoIosArrowBack
+            size={52}
+            color={`${selectedBenefit === 0 ? "transparent" : ""}`}
           />
         </span>
-        {benefits.map(benefit => (
-          <article 
-            key={benefit.title} 
-            className={`sm:w-52 sm:flex sm:flex-col sm:items-center ${selectedBenefit === benefit.id 
-              ? 'w-52 flex flex-col items-center mx-2' 
-              : 'hidden'}`}
+        {benefits.map((benefit) => (
+          <article
+            key={benefit.title}
+            className={`sm:w-52 sm:flex sm:flex-col sm:items-center ${
+              selectedBenefit === benefit.id
+                ? "w-52 flex flex-col items-center mx-2"
+                : "hidden"
+            }`}
           >
-            <span className="h-52 w-52 p-10 mb-6 flex justify-center items-center rounded-full border border-gray">
-              <img src={benefit.icon} />
+            <span className="h-40 w-40 sm:h-52 sm:w-52 p-6 mb-6 flex justify-center items-center rounded-full border border-gray">
+              <img src={benefit.icon} alt="benefit-icon" />
             </span>
             <h4 className="mb-6 text-title2 text-center font-semibold">
               {benefit.title}
             </h4>
             <p>{benefit.description}</p>
           </article>
-        ))
-        }
-        <span 
-          className="sm:hidden hover:cursor-pointer" 
+        ))}
+        <span
+          className="sm:hidden hover:cursor-pointer"
           onClick={() => {
-            if(selectedBenefit < benefits.length - 1) setSelectedBenefit(selectedBenefit + 1)}}
+            if (selectedBenefit < benefits.length - 1)
+              setSelectedBenefit(selectedBenefit + 1)
+          }}
         >
-          <IoIosArrowForward 
-            size={52} 
-            color={`${selectedBenefit === benefits.length - 1 
-              ? 'transparent'
-              : ''
+          <IoIosArrowForward
+            size={52}
+            color={`${
+              selectedBenefit === benefits.length - 1 ? "transparent" : ""
             }`}
           />
         </span>
@@ -58,4 +59,4 @@ const BenefitsSection = () => {
   )
 }
 
-export default BenefitsSection;
+export default BenefitsSection
