8539d51c8e62a29db8e11f2cf5cb70a18d629314
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2022-10-10 22:55:41 -0400

    refactor: Register connection with API
    Frontend register form connected to API and userExist functions creation
    
    Connect Frontend-Backend Register (Backend)

--- frontend/src/Components/Register/RegisterForm.jsx
+++ frontend/src/Components/Register/RegisterForm.jsx
@@ -1,7 +1,49 @@
 import { UtilForm } from "@/Utils"
+import { useState } from "react"
 import logo from "@/assets/AppIcon.webp"
 
 function RegisterForm() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    //alert("User Added")
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+    }
+
+    try {
+      const add = await fetch("http://localhost:4000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      }).then((res) => {
+        return res.json()
+      })
+
+      console.log(add)
+    } catch (err) {
+      console.error()
+    }
+  }
+
   return (
     <section className="px-5 mx-auto md:mx-[5%] ">
       <aside>
@@ -13,18 +55,68 @@ function RegisterForm() {
             Crear una nueva cuenta
           </h1>
         </div>
-        <form action="">
+
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label className="text-footer font-semibold text-[#8E9093] tracking-wide">
+              Nombre
+              <input
+                onChange={handleName}
+                className="w-full text-call text-black py-2 bg-inherit border-b border-black focus:outline-none focus:border-primary-light"
+                type="text"
+                required
+              />
+            </label>
+          </div>
+          <div className="mb-6">
+            <label className="text-footer font-semibold text-[#8E9093] tracking-wide">
+              Email
+              <input
+                onChange={handleEmail}
+                className="w-full text-call text-black py-2 bg-inherit border-b border-black focus:outline-none focus:border-primary-light"
+                type="email"
+                required
+              />
+            </label>
+          </div>
+          <div className="mt-6">
+            <label className="text-footer font-semibold text-[#8E9093] tracking-wide">
+              Password
+              <input
+                onChange={handlePassword}
+                className="w-full text-call text-black py-2 bg-inherit border-b border-black focus:outline-none focus:border-primary-light"
+                type="password"
+                required
+              />
+            </label>
+          </div>
+
+          <div className="mt-10">
+            <button
+              className="bg-primary-light text-gray-100 py-2 w-full rounded-lg tracking-widest
+                                font-semibold font-title focus:outline-none focus:shadow-outline hover:bg-primary-darker
+                                shadow-lg"
+            >
+              Crear Cuenta
+            </button>
+          </div>
+        </form>
+
+
+
+        {/* <form onSubmit={handleSubmit} action="">
           <div className="mt-8 md:mt-12 flex flex-col gap-3 ">
-            <UtilForm name="name" type="text">
+            <UtilForm onChange={handleName} name="name" type="text">
               Nombre Completo
             </UtilForm>
-            <UtilForm name="email" type="email">
+            <UtilForm onChange={handleEmail} name="email" type="email">
               Email
             </UtilForm>
-            <UtilForm name="password" type="password">
+            <UtilForm onChange={handlePassword} name="password" type="password">
               Contraseña
             </UtilForm>
-            <UtilForm name="confirmpass" type="password">
+            <UtilForm name="confirmpass" type="password" >
               Confirmar Contraseña
             </UtilForm>
           </div>
@@ -36,7 +128,8 @@ function RegisterForm() {
               Crear cuenta
             </button>
           </span>
-        </form>
+        </form> */}
+
       </aside>
     </section>
   )
