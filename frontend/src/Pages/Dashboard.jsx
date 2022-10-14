dc36e6518e229079e02968e05a0139eb126a5e97
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-13 22:22:49 -0400

    feat: Add Dashboard & Sidebar

---
+++ frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/Components"
+
+function Dashboard() {
+  return (
+    <>
+      <Sidebar />
+      Dashboard
+    </>
+  )
+}
+export default Dashboard
---
+++ frontend/src/helpers/dashboard.js
@@ -0,0 +1,13 @@
+import Dashboard from "@/assets/images/dashboard-icons/fa_home.svg"
+import Statistics from "@/assets/images/dashboard-icons/statistics.svg"
+import Money from "@/assets/images/dashboard-icons/money.svg"
+import Savings from "@/assets/images/dashboard-icons/piggy-bank.svg"
+import Expenses from "@/assets/images/dashboard-icons/people-team.svg"
+
+export const dashLinks = [
+  { id: 1, name: "Dashboard", link: "/", icon: Dashboard },
+  { id: 2, name: "Estadísticas", link: "/", icon: Statistics },
+  { id: 3, name: "Compromisos", link: "/", icon: Money },
+  { id: 4, name: "Ahorros", link: "/", icon: Savings },
+  { id: 5, name: "Dividir Gastos", link: "/", icon: Expenses },
+]
