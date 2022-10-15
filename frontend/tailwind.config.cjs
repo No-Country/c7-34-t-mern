feb86566a59579d910170d83e3fafacebe33efb1
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-15 17:34:48 -0400

    chore: Add Image & change style config
    Change tailwind config & index css styles

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     fontSize: {
       bold_title: "2.13rem",
       big_title: "2.13rem",
