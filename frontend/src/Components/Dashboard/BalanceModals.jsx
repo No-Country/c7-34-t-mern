cc1b93d978889e7b1b3abc0212a76c07666d0ce2
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-19 13:23:53 -0400

    refactor: Add loader & change format

--- frontend/src/Components/Dashboard/BalanceModals.jsx
+++ frontend/src/Components/Dashboard/BalanceModals.jsx
@@ -1,38 +1,36 @@
 import { ImCross } from "react-icons/im"
 import { ExpenseForm, IncomeForm } from ".."
 
-const BalanceModals = ({ 
-  postBalance, 
-  setPostBalance, 
-  movements, 
-  setMovements 
-}) => {
+function BalanceModals({
+  postBalance,
+  setPostBalance,
+  movements,
+  setMovements,
+}) {
   return (
     <div className="fixed top-0 right-0 h-screen w-screen sm:w-[30rem] bg-white rounded-lg">
       <div className="relative">
         <span
-          className="absolute top-4 left-24 sm:top-4 sm:left-4"
+          className="absolute top-4 left-4"
           onClick={() => setPostBalance(null)}
         >
           <ImCross size={30} color="black" />
         </span>
         <div className="h-full w-full p-20 flex flex-col justify-center items-center">
-          { postBalance == "income" 
-            ? <IncomeForm  
-                movements={movements} 
-                setMovements={setMovements} 
-                setPostBalance={setPostBalance}
-              /> 
-            : null
-          }
-          { postBalance == "expense" 
-            ? <ExpenseForm  
-                movements={movements} 
-                setMovements={setMovements} 
-                setPostBalance={setPostBalance}
-              /> 
-            : null
-          }
+          {postBalance == "income" ? (
+            <IncomeForm
+              movements={movements}
+              setMovements={setMovements}
+              setPostBalance={setPostBalance}
+            />
+          ) : null}
+          {postBalance == "expense" ? (
+            <ExpenseForm
+              movements={movements}
+              setMovements={setMovements}
+              setPostBalance={setPostBalance}
+            />
+          ) : null}
         </div>
       </div>
     </div>
