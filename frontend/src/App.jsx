42a2d6f6966c46ba982f6cdbb9cb1bde07fec61b
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-19 00:40:04 -0400

    feat: Add Error page
    Error page added when write a wrong route
    
    Error Page (FE)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect, useState } from "react"
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Loader } from "./Components"
 
@@ -6,6 +6,7 @@ const Dashboard = lazy(() => import("@/Pages/Dashboard"))
 const Home = lazy(() => import("@/Pages/Home"))
 const SignIn = lazy(() => import("@/Pages/SignIn"))
 const SignUp = lazy(() => import("@/Pages/SignUp"))
+const Error = lazy(() => import("@/Pages/Error"))
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route exact path="login" element={<SignIn />} />
             <Route exact path="register" element={<SignUp />} />
             <Route exact path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </BrowserRouter>
       </Suspense>
